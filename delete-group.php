f49b2ebb90595f618880f324304ae77f4c836607
Grisha Ivanetc <59262523+deCodedLife@users.noreply.github.com>
2019-12-26 22:41:58 +0300

    Update delete-group.php

--- delete-group.php
+++ delete-group.php
@@ -34,7 +34,7 @@ for ($i = 0; $i < count($agname); $i++)
 
 if ( empty($errors) )
 {
-    $connect = mysqli_connect("localhost", "id10026645_administrator", "8895304025dr", "id10026645_school");
+    $connect = mysqli_connect("localhost", "your_database_login", "your_database_password", "database_name");
     $request1 = mysqli_query  ($connect, "select * FROM `users` WHERE name = \"$login\";");
     $request2 = mysqli_query  ($connect, "select * FROM `groups` WHERE name = \"$gname\"");
     $res1 = mysqli_fetch_assoc($request2);
