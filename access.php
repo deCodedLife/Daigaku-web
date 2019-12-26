aa06fa34d2dcfa26bd5ee1bd0287b0e37529b2b6
Grisha Ivanetc <59262523+deCodedLife@users.noreply.github.com>
2019-12-26 22:37:09 +0300

    Update access.php

--- access.php
+++ access.php
@@ -38,7 +38,7 @@ for ($i = 0; $i < count($apassw); $i++)
 
 if (empty($errors) )
 {
-    $connect = mysqli_connect("localhost", "id10026645_administrator", "8895304025dr", "id10026645_school");
+    $connect = mysqli_connect("localhost", "your_database_login", "your_database_password", "database_name");
     $request = mysqli_query  ($connect, "select * FROM `users` WHERE name = \"$login\"");
     $res = mysqli_fetch_assoc($request);
     if ( $login != $res["name"] )  $errors[] = "Вас нет в списке пользователей";
